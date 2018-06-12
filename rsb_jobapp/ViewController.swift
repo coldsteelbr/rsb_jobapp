b77695b03d2d45384491006401d9865d992821aa
Roman <cold.steel.br@gmail.com>
2018-06-12 12:50:16 +0700

    Initial Commit

---
+++ rsb_jobapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rsb_jobapp
+//
+//  Created by Roman Brazhnikov on 12.06.2018.
+//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
