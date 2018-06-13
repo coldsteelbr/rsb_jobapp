305b44c844a715578f07f677f3d48a5554042c6a
Roman <cold.steel.br@gmail.com>
2018-06-13 20:55:01 +0700

    Rid of status bar overlap

--- rsb_jobapp/vacancyList/VacancyListVC.swift
+++ rsb_jobapp/vacancyList/VacancyListVC.swift
@@ -19,6 +19,11 @@ class VacancyListVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+        
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 65
     }
